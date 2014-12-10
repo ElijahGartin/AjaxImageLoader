783b2d7b7520becb6b6a53ea84ca1c3ef4c8404d
EGartin <elijah.gartin@gmail.com>
2014-12-09 21:09:50 -0700

    Fixed script to utilize a different jQuery method to replace image in script and removed some of the bloat in the ajaximageloader.js script

--- js/ajaximageloader.js
+++ js/ajaximageloader.js
@@ -9,11 +9,8 @@ $( document ).ready(function() {
 	
 	var self = this;
 	var url = "";
-	var flag;
-	
 	
 		$( "#largeClick" ).on( "click", function( event ) {
-			var flag = 1;
 			$.ajax({
 				type: "GET",
 				url: "img/KunTaoRevolver.gif",
@@ -23,32 +20,18 @@ $( document ).ready(function() {
 		});//end .on click, function event
 	
 		$( "#smallClick" ).on( "click", function( event ) {
-			var flag = 0;
 			$.ajax({
 				type: "GET",
 				url: "img/KunTaoRevolver_200h.gif",
 				success: showImage()
 			});//end of .ajax call
 			url = "img/KunTaoRevolver_200h.gif";
+			
 		});//end .on click, function event	
-	
-	//getURL() Function not currently working - needs more thought
-/*		function getURL(){
-			if (flag < 1){
-				url = "img/KunTaoRevolver_200h.gif";
-				return "img/KunTaoRevolver_200h.gif";
-			}
-			else if (flag >= 1){
-				url = "img/KunTaoRevolver.gif";
-				return "img/KunTaoRevolver.gif";
-				
-			}//end if
-			console.log("getURL ran");	
-		}//end of function get URL
-*/	
+
 		function showImage() {
 			setTimeout(function(){///this function fire after 1ms delay
-      				$('<img src="'+ url +'" />').appendTo( "#imageGIF" );
+      				$('#imageGIF').attr('src',url);
 		   			$('#LinkToSite').show();
 			}, 1);
 			
