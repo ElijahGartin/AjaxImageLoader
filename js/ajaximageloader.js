02c9632b69d63f0b12bcc26ec70106cc2613dd10
EGartin <elijah.gartin@gmail.com>
2014-12-17 19:31:48 -0700

    Updated the javascript/jQuery logic and execution.  Debounce was working locally, not working on a remote server.

--- js/ajaximageloader.js
+++ js/ajaximageloader.js
@@ -1,7 +1,8 @@
 /*
 Author: Elijah Gartin
 Date: 2014-12-09
-Description: Ajax Image Loader.  It creates an image request after a button is clicked.
+Updated: 2014-12-17
+Description: Ajax Image Loader.  It creates an image request after a button is clicked.  It controls the allowed amount of requests by utilizing a debounce method.
 */
 
 $( document ).ready(function() {
@@ -10,35 +11,38 @@ $( document ).ready(function() {
 	var self = this;
 	var url = "";
 	
-	/*EXAMPLE DEBOUNCE METHOD
-	$('input.text').keyup( $.debounce( 250, text_2 ) );
-	*/
+	//Large GIF Action ******/
+	$( "#largeClick" ).on( "click", function( event ) {
+		$.debounce(large_ajax_call(), 5000 )
+	});//end .on click, function event
 	
-	//Most likely will need to fix this function to narrow it down to one ajax call and it firing off the debounce method
-	function ajax_call( event ) {
-		//Large GIF AJAX Call
-		$( "#largeClick" ).on( "click", function( event ) {
-			$.ajax({
-				type: "GET",
-				url: "img/KunTaoRevolver.gif",
-				success: showImage()
-			});//end of .ajax call
-			url = "img/KunTaoRevolver.gif";
-		});//end .on click, function event
-		
-		//Small GIF AJAX Call
-		$( "#smallClick" ).on( "click", function( event ) {
-			$.ajax({
-				type: "GET",
-				url: "img/KunTaoRevolver_200h.gif",
-				success: showImage()
-			});//end of .ajax call
-			url = "img/KunTaoRevolver_200h.gif";
-				
-			});//end .on click, function event			
-	};
+	//Small GIF Action ******/
+	$( "#smallClick" ).on( "click", function( event ) {
+		$.debounce(small_ajax_call(), 5000 )
+	});//end .on click, function event
+	
+	//Large GIF AJAX Call
+	function large_ajax_call() {
+		$.ajax({
+			type: "GET",
+			url: "img/KunTaoRevolver.gif",
+			success: showImage()
+		});//end of .ajax call
+		url = "img/KunTaoRevolver.gif";
+	};//end large_ajax_call function event
+	
+	//Small GIF AJAX Call
+	function small_ajax_call() {
+		$.ajax({
+			type: "GET",
+			url: "img/KunTaoRevolver_200h.gif",
+			success: showImage()
+		});//end of .ajax call
+		url = "img/KunTaoRevolver_200h.gif";
+			
+	};//end small_ajax_call function event			
 
-
+	//This Function will change the source in the img tag and change it's status from hide() to show().
 	function showImage() {
 		setTimeout(function(){///this function fire after 1ms delay
 				$('#imageGIF').attr('src',url);
@@ -46,6 +50,5 @@ $( document ).ready(function() {
 		}, 1); 
 	}//end function showImage()
 	
-
 });//end document ready
 	
